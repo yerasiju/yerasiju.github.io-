a299727fc0d8e98d165f4ef2952bbc698d610f50
yerasiju <erkebulan.zholymbek@bk.ru>
2024-03-14 14:59:52 +0600

    hi

---
+++ 01/app.js
@@ -0,0 +1,55 @@
+let num = 42;
+let firstName = "Vladilen";
+const isProgrammer = true;
+
+firstName = "Max ";
+
+// alert(firstName)
+
+// console.log('test :', firstName)
+
+// isProgrammer = false // wrong because of cons(constant)
+//.
+
+/* ARITHMETICAL TYPES
+console.log(num + 53);
+console.log(num - 53);
+console.log(num * 53);
+console.log(num / 53);
+console.log(num % 53);
+console.log(num ** 3); */
+
+/*let num2 = num + 10;
+console.log(num, num2);
+num = num2 - num;
+console.log(num, num2);
+
+*/
+
+//.
+
+/*Can do
+let $ = 'test'
+let $num = 42
+let num$ = 42
+let _ = 49
+let num_ = 12
+let _num = 12
+let first_Name = 'Elena'
+let myNum = 34
+let num42 = 10 */
+
+/* Restricted WRONG
+let 42num = '11'
+let my-num = 11
+let const
+*/
+
+//let num3 = (num + 10 * 2) / (5 - 1);
+//console.log(num3);
+/* const fullName = firstName + "Minin" == VladilenMinin;
+const fullName = firstName + " Minin"; == Vladilen Minin
+const fullName = firstName + ' ' + "Minin"; == Vladilen Minin
+const fullName = firstName + '/n' + "Minin"; == 
+ Vladilen
+ Minin */
---
+++ web/web.js
@@ -0,0 +1,12 @@
+/*function addVariable(a,b) {
+	return a+b
+}
+const a = 5
+const b = 7
+
+console.log(addVariable(a,b))*/
+const list = [1, 2, 3, 4, 5, 5, 6];
+console.log(list[1]);
+// length outputqa 7 shygarady
+// join('*') output : 1 * 2 * 3 * 4 * 5 * 5* 6
+// list[1] output : 2
